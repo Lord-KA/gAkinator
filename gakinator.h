1e112e48b60924f1d645785825550df138c35b08
Lord <lord.nemo@protonmail.com>
2021-11-28 15:05:07 +0300

    initial commit

---
+++ gakinator.h
@@ -0,0 +1,24 @@
+#pragma once
+
+struct gAkinator_Node   //TODO
+{    
+    int smth = 0;
+} typedef gAkinator_Node;
+
+typedef gAkinator_Node GTREE_TYPE;
+
+#include "gtree.h"
+
+bool gTree_storeData(gAkinator_Node *data, size_t level, FILE *out)         //TODO
+{
+    return 0;
+}
+
+bool gTree_restoreData(gAkinator_Node data, FILE *in)                       //TODO
+{
+    return 0;
+}
+
+struct gAkinator {
+
+} typedef gAkinator;
