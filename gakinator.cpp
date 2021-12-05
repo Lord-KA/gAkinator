f3a6866e48f7de7747a1e82bc46f6e1677481768
Lord <lord.nemo@protonmail.com>
2021-12-05 23:15:10 +0300

    redone cli interface

--- gakinator.cpp
+++ gakinator.cpp
@@ -3,16 +3,74 @@
 #include "gakinator.h"
 #include <cstdlib>
 
-void helpMsg() 
+
+gAkinator_status gAkinator_interface_play(gAkinator *akinator) 
 {
-    fprintf(stdout, "commands:\n");
-    fprintf(stdout, "\t1. play              - play the guessing game\n");
-    fprintf(stdout, "\t2. compare <id> <id> - comapare two characters\n");
-    fprintf(stdout, "\t3. define  <id>      - print the definition of a character\n");
-    fprintf(stdout, "\t4. show              - show the whole graph of questions and answers\n");
-    fprintf(stdout, "\t5. quit              - exit the program\n");
-    fprintf(stdout, "\t6. help              - print this help\n");
+    return gAkinator_game(akinator);
 }
+
+gAkinator_status gAkinator_interface_comp(gAkinator *akinator) 
+{
+    size_t   oneNodeId = -1;
+    size_t otherNodeId = -1;
+    printf("Please provide two node ids:\n");
+    if (scanf("%lu %lu", &oneNodeId, &otherNodeId) != 2) {
+        fprintf(stdout, "Bad command provided, please try again!\n");
+    }
+    return gAkinator_comp(akinator, oneNodeId, otherNodeId);
+}
+
+gAkinator_status gAkinator_interface_defn(gAkinator *akinator) 
+{
+    size_t nodeId = -1;
+    printf("Please provide nodeId:\n");
+    if (scanf("%lu", &nodeId) != 1) {
+        fprintf(stdout, "Bad command provided, please try again!\n");
+    }
+    return gAkinator_definition(akinator, nodeId);
+}
+
+gAkinator_status gAkinator_interface_show(gAkinator *akinator) 
+{
+    FILE *out = fopen("graph.gv", "w");
+    if (gTree_dumpPoolGraphViz(&akinator->tree, out) != gTree_status_OK)
+        return gAkinator_status_TreeErr;
+    fclose(out);
+    system("dot -Tpdf -ograph.pdf ./graph.gv && zathura ./graph.pdf 2> /dev/null");
+    return gAkinator_status_OK;
+}
+
+gAkinator_status gAkinator_interface_quit(gAkinator *akinator) 
+{
+    return gAkinator_status_OK;
+}
+
+gAkinator_status gAkinator_interface_help(gAkinator *akinator);
+
+struct Command {
+    const char *name;
+    const char *descr;
+    gAkinator_status (*func)(gAkinator *akinator);
+};
+
+static const Command cmds[] {
+    Command{.name = "play   ", .descr = "play the guessing game",  .func = &gAkinator_interface_play},
+    Command{.name = "compare", .descr = "comapare two characters", .func = &gAkinator_interface_comp},
+    Command{.name = "define ", .descr = "print the definition of a character", .func = &gAkinator_interface_defn},
+    Command{.name = "show   ", .descr = "show the whole graph of questions and answers", .func = &gAkinator_interface_show},
+    Command{.name = "quit   ", .descr = "exit the program",        .func = &gAkinator_interface_quit},
+    Command{.name = "help   ", .descr = "show this help",          .func = &gAkinator_interface_help},
+};
+
+gAkinator_status gAkinator_interface_help(gAkinator *akinator) 
+{
+    printf("commands:\n");
+    for (size_t i = 0; i < sizeof(cmds) / sizeof(cmds[0]); ++i) 
+        printf("%d.  %s - %s\n", i + 1, cmds[i].name, cmds[i].descr);
+
+    return gAkinator_status_OK;
+}
+
 
 int main()
 {
@@ -21,7 +79,6 @@ int main()
     #ifdef EXTRA_VERBOSE
         fprintf(stderr, "EXTRA_VERBOSE is defined!!!!\n");
     #endif
-    // FILE *in = fopen("../examples/node-adding.gt", "r");
     FILE *in = fopen("../examples/not-that-simple.gt", "r");
     gAkinator_ctor(akinator, stderr, in);
     fclose(in);
@@ -29,38 +86,24 @@ int main()
     fprintf(stdout, "==============================\n");
     fprintf(stdout, "== Akinator game by Lord_KA ==\n");
     fprintf(stdout, "==============================\n");
-    helpMsg();
+
+    gAkinator_interface_help(akinator);
+
     char command[MAX_LINE_LEN] = "";
     while (!strIsQuit(command)) {
         getline(command, MAX_LINE_LEN, stdin);
-        if (!strSkpCmp(command, "play", 1)) {
-            gAkinator_game(akinator);
-        } else if (!strnSkpCmp(command, "compare", 7)) {
-            size_t   oneNodeId = -1;
-            size_t otherNodeId = -1;
-            if (sscanf(command, "compare %lu %lu ", &oneNodeId, &otherNodeId) != 2) {
-                fprintf(stdout, "Bad command provided, please try again!\n");
-                continue;
+        bool foundCommand = false;
+        for (size_t i = 0; i < sizeof(cmds) / sizeof(cmds[0]); ++i) {
+            if (strIsComb(command, cmds[i].name)) {
+                foundCommand = true;
+                if ((*cmds[i].func)(akinator) != gAkinator_status_OK) {
+                    fprintf(stderr, "Fatal error during execution\n");
+                    exit(1);
+                }
             }
-            gAkinator_comp(akinator, oneNodeId, otherNodeId);
-        } else if (!strnSkpCmp(command, "define", 6)) {
-            size_t nodeId = -1;
-            if (sscanf(command, "define %lu ", &nodeId) != 1) {
-                fprintf(stdout, "Bad command provided, please try again!\n");
-                continue;
-            }
-            gAkinator_definition(akinator, nodeId);
-        } else if (!strSkpCmp(command, "show", 1)) {
-            FILE *out = fopen("graph.gv", "w");
-            gTree_dumpPoolGraphViz(&akinator->tree, out);
-            fclose(out);
-            system("dot -Tpdf -ograph.pdf ./graph.gv && zathura ./graph.pdf 2> /dev/null");
-        } else if (!strSkpCmp(command, "help", 1)) {
-            helpMsg();
-        } else if (!strIsQuit(command)) {
-            fprintf(stdout, "Bad command provided, please try again!\n");
-            continue;
         }
+        if (!foundCommand) 
+            fprintf(stdout, "Bad command provided, please try again!\n");
     }
 
     gAkinator_dtor(akinator);
